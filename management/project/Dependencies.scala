f14fafd6df887011af1d77dac8070330ddee21da
Milenio X <jnkmiloq@gmail.com>
2023-05-24 22:00:31 -0500

    Added Jackson serialization dependency to handle JSON in the API

--- management/project/Dependencies.scala
+++ management/project/Dependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
@@ -7,20 +7,21 @@ object Dependencies {
   lazy val log4jVersion = "2.19.0"
   lazy val akkaVersion = "2.7.0"
   lazy val akkaHttpVersion = "10.5.1"
+  lazy val jacksonVersion = "2.15.1"
 
-  lazy val log4jScala = "org.apache.logging.log4j" % "log4j-api-scala_2.13" % "12.0"
-  lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % "2.19.0" % Runtime
+  lazy val log4jScala = "org.apache.logging.log4j" % "log4j-api-scala_2.13" % scalaLog4jVersion
+  lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % log4jVersion % Runtime
   lazy val akkaActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   lazy val akkaHttp =  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
+  lazy val jacksonModule = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
 
-  lazy val portsDependencies = Seq(
-    akkaActorTyped,
-    akkaStream,
-    akkaHttp
+  lazy val portsDependencies: Seq[ModuleID] = Seq(
+    akkaActorTyped, akkaStream, akkaHttp,
+    jacksonModule
   )
 
-  lazy val commonDependencies = Seq(
+  lazy val commonDependencies: Seq[ModuleID] = Seq(
     log4jScala, log4jCore,
     scalaTest % Test
   )
