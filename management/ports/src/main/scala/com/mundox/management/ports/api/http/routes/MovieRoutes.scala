adcbec0ebe16126e1ac704344bc089f7a02aa26e
Milenio X <jnkmiloq@gmail.com>
2023-05-24 22:01:23 -0500

    GET Method implemented to return a list of dummy movies

--- management/ports/src/main/scala/com/mundox/management/ports/api/http/routes/MovieRoutes.scala
+++ management/ports/src/main/scala/com/mundox/management/ports/api/http/routes/MovieRoutes.scala
@@ -3,7 +3,11 @@ package com.mundox.management.ports.api.http.routes
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.mundox.management.core.env.log.Logger
+import com.mundox.management.ports.api.http.responses.DummyMovieResponseDTO
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
@@ -19,14 +23,23 @@ class MovieRoutes extends Logger {
     path("movies") {
       get {
         loggerInfo("getMovies service invoked")
-        val result = Await.ready(Future {
-          Some(List(1))
+        val result: Future[List[DummyMovieResponseDTO]] = Await.ready(Future {
+          List(
+            DummyMovieResponseDTO("123", "Movie 1"),
+            DummyMovieResponseDTO("321", "Movie 2")
+          )
         }, 10.seconds)
 
         onComplete(result) {
           case Success(value) =>
+            val mapper = JsonMapper
+              .builder()
+              .addModule(DefaultScalaModule)
+              .enable(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES)
+              .build()
+
             loggerInfo("success response in getMovies service")
-            complete(StatusCodes.OK, value.toString)
+            complete(StatusCodes.OK -> mapper.writeValueAsString(value))
           case Failure(ex) =>
             loggerError(s"getMovies service has an error: $ex")
             complete(StatusCodes.InternalServerError, ex.getMessage)
