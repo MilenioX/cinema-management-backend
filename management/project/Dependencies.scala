ac160e4ed579fefe4ae15aa92052fd85a0f0d937
Milenio X <jnkmiloq@gmail.com>
2023-05-27 00:11:12 -0500

    Remove jackson dependency

--- management/project/Dependencies.scala
+++ management/project/Dependencies.scala
@@ -7,19 +7,17 @@ object Dependencies {
   lazy val log4jVersion = "2.19.0"
   lazy val akkaVersion = "2.7.0"
   lazy val akkaHttpVersion = "10.5.1"
-  lazy val jacksonVersion = "2.15.1"
 
   lazy val log4jScala = "org.apache.logging.log4j" % "log4j-api-scala_2.13" % scalaLog4jVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % log4jVersion % Runtime
   lazy val akkaActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   lazy val akkaHttp =  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
-  lazy val jacksonModule = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
   lazy val sprayJsonAkka = "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 
   lazy val portsDependencies: Seq[ModuleID] = Seq(
     akkaActorTyped, akkaStream, akkaHttp,
-    jacksonModule, sprayJsonAkka,
+    sprayJsonAkka,
   )
 
   lazy val commonDependencies: Seq[ModuleID] = Seq(
