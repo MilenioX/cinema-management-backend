3d5fdee944cfbb5765d6f3d562b71435cd60d9c8
Milenio X <jnkmiloq@gmail.com>
2023-05-28 00:22:05 -0500

    Added Scala test kit for Akka Http and Akka streams

--- management/project/Dependencies.scala
+++ management/project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 
   lazy val scalaLog4jVersion = "12.0"
   lazy val log4jVersion = "2.19.0"
@@ -12,11 +12,14 @@ object Dependencies {
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % log4jVersion % Runtime
   lazy val akkaActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
+  lazy val akkaStreamTest = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion
   lazy val akkaHttp =  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
+  lazy val akkaHttpTest = "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion
   lazy val sprayJsonAkka = "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 
   lazy val portsDependencies: Seq[ModuleID] = Seq(
     akkaActorTyped, akkaStream, akkaHttp,
+    akkaStreamTest, akkaHttpTest,
     sprayJsonAkka,
   )
 
