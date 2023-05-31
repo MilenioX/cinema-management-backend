db5dd4ba0a275369d6be8eb7cf47fa7b2c4e0726
Milenio X <jnkmiloq@gmail.com>
2023-05-31 12:06:30 -0500

    Added Cats to the project to handle some typeClasses

--- management/project/Dependencies.scala
+++ management/project/Dependencies.scala
@@ -5,11 +5,13 @@ object Dependencies {
 
   lazy val scalaLog4jVersion = "12.0"
   lazy val log4jVersion = "2.19.0"
+  lazy val catsVersion = "2.9.0"
   lazy val akkaVersion = "2.7.0"
   lazy val akkaHttpVersion = "10.5.1"
 
   lazy val log4jScala = "org.apache.logging.log4j" % "log4j-api-scala_2.13" % scalaLog4jVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % log4jVersion % Runtime
+  lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
   lazy val akkaActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   lazy val akkaStreamTest = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion
@@ -18,6 +20,7 @@ object Dependencies {
   lazy val sprayJsonAkka = "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 
   lazy val portsDependencies: Seq[ModuleID] = Seq(
+    cats,
     akkaActorTyped, akkaStream, akkaHttp,
     akkaStreamTest, akkaHttpTest,
     sprayJsonAkka,
