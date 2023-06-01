12eb95d573997ade05ac098d59377a10d2bc6de2
Milenio X <jnkmiloq@gmail.com>
2023-06-01 18:30:18 -0500

    Added Mockito as dependency to mock classes

--- management/project/Dependencies.scala
+++ management/project/Dependencies.scala
@@ -1,8 +1,10 @@
 import sbt.*
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 
+
+  lazy val scalaTestVersion = "3.2.16"
+  lazy val mockitoVersion = "3.2.15.0"
   lazy val scalaLog4jVersion = "12.0"
   lazy val log4jVersion = "2.19.0"
   lazy val catsVersion = "2.9.0"
@@ -11,6 +13,8 @@ object Dependencies {
 
   lazy val log4jScala = "org.apache.logging.log4j" % "log4j-api-scala_2.13" % scalaLog4jVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % log4jVersion % Runtime
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
+  lazy val mockito = "org.scalatestplus" %% "mockito-4-6" % mockitoVersion % Test
   lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
   lazy val akkaActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
   lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
@@ -28,6 +32,6 @@ object Dependencies {
 
   lazy val commonDependencies: Seq[ModuleID] = Seq(
     log4jScala, log4jCore,
-    scalaTest % Test
+    scalaTest, mockito,
   )
 }
