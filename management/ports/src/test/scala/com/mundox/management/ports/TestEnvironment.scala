e11902627987191b0abfb74e8ad4dc88ebbd2032
Milenio X <jnkmiloq@gmail.com>
2023-06-02 15:40:29 -0500

    Fixed unit tests with the new EitherT definition

--- management/ports/src/test/scala/com/mundox/management/ports/TestEnvironment.scala
+++ management/ports/src/test/scala/com/mundox/management/ports/TestEnvironment.scala
@@ -1,7 +1,10 @@
 package com.mundox.management.ports
 
+import cats.data.EitherT
 import com.mundox.management.core.domain.DummyMovie
+import com.mundox.management.core.exceptions.ManagementException
 import com.mundox.management.core.services.DummyMoviesService
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar.mock
 
@@ -12,6 +15,11 @@ class TestEnvironment extends Environment {
   val serviceMock: DummyMoviesService = mock[DummyMoviesService]
   when(serviceMock.getMovies).thenReturn {
     Future.successful {Right(List(DummyMovie("123", "Movie 1"),DummyMovie("321", "Movie 2")))}
+  }
+  when(serviceMock.addMovie(any())).thenReturn {
+    EitherT[Future, ManagementException, Option[DummyMovie]] {
+      Future.successful {Right(Option(DummyMovie("912", "Testing Movie")))}
+    }
   }
 
   override val dummyMoviesService: DummyMoviesService = serviceMock
