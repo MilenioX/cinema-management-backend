e6ad0c8a0530a1ba5f590e09187ec0e78f343c0e
Milenio X <jnkmiloq@gmail.com>
2023-06-02 14:09:23 -0500

    Implementation in the DTO Object to return an EitherT

--- management/ports/src/main/scala/com/mundox/management/ports/api/http/requests/DummyCreateMovieRequestDTO.scala
+++ management/ports/src/main/scala/com/mundox/management/ports/api/http/requests/DummyCreateMovieRequestDTO.scala
@@ -1,9 +1,14 @@
 package com.mundox.management.ports.api.http.requests
 
+import cats.data.EitherT
 import cats.implicits._
+import com.mundox.management.core.domain.DummyMovie
 import com.mundox.management.core.exceptions.{ManagementException, ValidationError}
 import com.mundox.management.core.validations.data.Validation
 import com.mundox.management.core.validations.validators.{Validator, ValidatorNec}
+
+import java.util.UUID
+import scala.concurrent.Future
 
 case class DummyCreateMovieRequestDTO(title: String)
 
@@ -13,12 +18,17 @@ object DummyCreateMovieRequestDTO {
       _ <- Validator.validateSpecialCharactersWithSpaces("title", input.title)
       _ <- Validator.validateMinLength("title", input.title, 5)
       title <- Validator.validateMaxLength("title", input.title, 20)
-    } yield {DummyCreateMovieRequestDTO(title)}).leftMap(v => ValidationError(List(v)))
+    } yield {
+      DummyCreateMovieRequestDTO(title)
+    }).leftMap(v => ValidationError(List(v)))
 
-  def validateNec(input: DummyCreateMovieRequestDTO): Either[ManagementException, DummyCreateMovieRequestDTO] =
-    (ValidatorNec.validateSpecialCharactersWithSpaces("title", input.title),
-      ValidatorNec.validateMinLength("title", input.title, 5),
-      ValidatorNec.validateMaxLength("title", input.title, 20))
-      .mapN((_, _, _) => DummyCreateMovieRequestDTO(input.title)).toEither.leftMap(validation => ValidationError(validation.toList))
-
+  def validateNec(input: DummyCreateMovieRequestDTO): EitherT[Future,ManagementException, DummyCreateMovieRequestDTO] =
+    EitherT {
+      Future.successful(
+        (ValidatorNec.validateSpecialCharactersWithSpaces("title", input.title),
+          ValidatorNec.validateMinLength("title", input.title, 5),
+          ValidatorNec.validateMaxLength("title", input.title, 20))
+          .mapN((_, _, _) => DummyCreateMovieRequestDTO(input.title)).toEither.leftMap(validation => ValidationError(validation.toList))
+      )
+    }
 }
