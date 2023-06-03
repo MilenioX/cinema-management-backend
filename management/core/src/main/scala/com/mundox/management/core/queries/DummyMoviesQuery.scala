b344c56de577a5360f1cbfb5303ad60338d83642
Milenio X <jnkmiloq@gmail.com>
2023-06-02 20:18:57 -0500

    Added log messages to track each invocation

--- management/core/src/main/scala/com/mundox/management/core/queries/DummyMoviesQuery.scala
+++ management/core/src/main/scala/com/mundox/management/core/queries/DummyMoviesQuery.scala
@@ -1,16 +1,21 @@
 package com.mundox.management.core.queries
 
 import com.mundox.management.core.domain.DummyMovie
+import com.mundox.management.core.env.log.Logger
 import com.mundox.management.core.exceptions.ManagementException
 import com.mundox.management.core.services.DummyMoviesService
 
 import scala.concurrent.Future
 
-class DummyMoviesQuery(service: DummyMoviesService) {
+class DummyMoviesQuery(service: DummyMoviesService) extends Logger {
 
-  def getMovies: Future[Either[ManagementException,List[DummyMovie]]] =
+  def getMovies: Future[Either[ManagementException,List[DummyMovie]]] = {
+    loggerInfo("getMovies service in query was invoked.")
     service.getMovies
+  }
 
-  def getMoviesById(id: String): Future[Either[ManagementException,Option[DummyMovie]]] =
+  def getMoviesById(id: String): Future[Either[ManagementException,Option[DummyMovie]]] = {
+    loggerInfo("getMoviesById service in query was invoked.")
     service.getMovieById(id)
+  }
 }
--- management/ports/src/main/scala/com/mundox/management/ports/adapters/DummyMoviesAdapter.scala
+++ management/ports/src/main/scala/com/mundox/management/ports/adapters/DummyMoviesAdapter.scala
@@ -2,6 +2,7 @@ package com.mundox.management.ports.adapters
 
 import cats.data.EitherT
 import com.mundox.management.core.domain.DummyMovie
+import com.mundox.management.core.env.log.Logger
 import com.mundox.management.core.exceptions.{AdapterError, ManagementException}
 import com.mundox.management.core.services.DummyMoviesService
 
@@ -9,7 +10,7 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 
-class DummyMoviesAdapter extends DummyMoviesService {
+class DummyMoviesAdapter extends DummyMoviesService with Logger {
 
   private var moviesList: List[DummyMovie] = List(
     DummyMovie("123", "Movie 1"),
@@ -18,49 +19,64 @@ class DummyMoviesAdapter extends DummyMoviesService {
     DummyMovie("789", "Movie 4")
   )
 
-  override def getMovies: Future[Either[ManagementException, List[DummyMovie]]] = Await.ready(Future {
-    try {
-      Right(moviesList)
-    } catch {
-      case e: Exception => Left(AdapterError(e.getMessage))
-    }
-  }, 10.seconds)
-
-  override def getMovieById(id: String): Future[Either[ManagementException, Option[DummyMovie]]] = Await.ready(Future {
-    try {
-      Right(moviesList.find(_.id.equals(id)))
-    } catch {
-      case e: Exception => Left(AdapterError(e.getMessage))
-    }
-  }, 5.seconds)
-
-  override def addMovie(newMovie: DummyMovie): EitherT[Future, ManagementException, Option[DummyMovie]] = EitherT {
-    Await.ready(Future[Either[ManagementException, Option[DummyMovie]]] {
+  override def getMovies: Future[Either[ManagementException, List[DummyMovie]]] = {
+    loggerInfo("getMovies service in adapter was invoked.")
+    Await.ready(Future {
       try {
-        moviesList = moviesList :+ newMovie
-        Right(Option(newMovie))
+        Right(moviesList)
+      } catch {
+        case e: Exception => Left(AdapterError(e.getMessage))
+      }
+    }, 10.seconds)
+  }
+
+  override def getMovieById(id: String): Future[Either[ManagementException, Option[DummyMovie]]] = {
+    loggerInfo("getMoviesById service in adapter was invoked.")
+    Await.ready(Future {
+      try {
+        Right(moviesList.find(_.id.equals(id)))
       } catch {
         case e: Exception => Left(AdapterError(e.getMessage))
       }
     }, 5.seconds)
   }
 
-  override def updateMovie(id: String, updatedMovie: DummyMovie): Future[Either[ManagementException, Option[DummyMovie]]] = Await.ready(Future {
-    try {
-      moviesList = moviesList.map(value => if (value.id.equals(id)) updatedMovie else value)
-      Right(Option(updatedMovie))
-    } catch {
-      case e: Exception => Left(AdapterError(e.getMessage))
+  override def addMovie(newMovie: DummyMovie): EitherT[Future, ManagementException, Option[DummyMovie]] = {
+    loggerInfo("addMovie service in adapter was invoked.")
+    EitherT {
+      Await.ready(Future[Either[ManagementException, Option[DummyMovie]]] {
+        try {
+          moviesList = moviesList :+ newMovie
+          Right(Option(newMovie))
+        } catch {
+          case e: Exception => Left(AdapterError(e.getMessage))
+        }
+      }, 5.seconds)
     }
-  }, 5.seconds)
+  }
 
-  override def deleteMovie(id: String): Future[Either[ManagementException, Option[DummyMovie]]] = Await.ready(Future {
-    try {
-      val deletedElement = moviesList.find(_.id.equals(id))
-      moviesList = moviesList.filter(!_.id.equals(id))
-      Right(deletedElement)
-    } catch {
-      case e: Exception => Left(AdapterError(e.getMessage))
-    }
-  }, 5.seconds)
+  override def updateMovie(id: String, updatedMovie: DummyMovie): Future[Either[ManagementException, Option[DummyMovie]]] = {
+    loggerInfo("updateMovie service in adapter was invoked.")
+    Await.ready(Future {
+      try {
+        moviesList = moviesList.map(value => if (value.id.equals(id)) updatedMovie else value)
+        Right(Option(updatedMovie))
+      } catch {
+        case e: Exception => Left(AdapterError(e.getMessage))
+      }
+    }, 5.seconds)
+  }
+
+  override def deleteMovie(id: String): Future[Either[ManagementException, Option[DummyMovie]]] = {
+    loggerInfo("deleteMovie service in adapter was invoked.")
+    Await.ready(Future {
+      try {
+        val deletedElement = moviesList.find(_.id.equals(id))
+        moviesList = moviesList.filter(!_.id.equals(id))
+        Right(deletedElement)
+      } catch {
+        case e: Exception => Left(AdapterError(e.getMessage))
+      }
+    }, 5.seconds)
+  }
 }
