01bdf4683f234d71fa7279e89b7088f5f3c2918f
Milenio X <jnkmiloq@gmail.com>
2023-07-23 22:21:34 -0500

    Refactored CommonValidations to use the Validator Object

--- management/ports/src/main/scala/com/mundox/management/ports/api/http/requests/CommonValidations.scala
+++ management/ports/src/main/scala/com/mundox/management/ports/api/http/requests/CommonValidations.scala
@@ -1,31 +1,17 @@
 package com.mundox.management.ports.api.http.requests
 
 import cats.implicits._
-import cats.data.Validated
 import com.mundox.management.core.exceptions.{ManagementException, ValidationError}
-import com.mundox.management.core.validations.data.ValueIsNotValid
+import com.mundox.management.core.validations.validators.Validator._
 import monix.eval.Task
 
 object CommonValidations {
 
   def validateId(id: String): Task[Either[ManagementException, Int]] =
     Task {
-      for {
-        idInt <- validateNumber(id)
-        _ <- validateIdGT0(idInt)
-      } yield idInt
+      (for {
+        idInt <- validateIsNumber("id", id)
+        _ <- validateNumberGreaterThanZero("id", idInt)
+      } yield idInt).leftMap(v => ValidationError(List(v)))
     }
-
-  def validateIdGT0(id: Int): Either[ManagementException, Int] =
-    Validated.condNec(id > 0, id, ValueIsNotValid("Error with the id"))
-        .toEither
-        .leftMap(v => ValidationError(v.toList))
-
-  def validateNumber(id: String): Either[ManagementException, Int] =
-      try {
-        Right(id.toInt)
-      } catch {
-        case _: Exception =>
-          Left(ValidationError(List(ValueIsNotValid("Error with the id"))))
-      }
 }
