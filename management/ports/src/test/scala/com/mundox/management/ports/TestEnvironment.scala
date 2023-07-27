1aa3fc4df7210bdee60ff1df3744516a8238d26f
Milenio X <jnkmiloq@gmail.com>
2023-07-27 09:13:54 -0500

    Fixed unit test implementation

--- management/ports/src/test/scala/com/mundox/management/ports/TestEnvironment.scala
+++ management/ports/src/test/scala/com/mundox/management/ports/TestEnvironment.scala
@@ -3,15 +3,15 @@ package com.mundox.management.ports
 import com.mundox.management.core.services.{DummyMoviesService, SnacksService}
 import com.mundox.management.ports.config.Environment
 import com.mundox.management.ports.mocks.adapters.DummyMoviesServiceMock.getMock
-
-import scala.concurrent.Future
+import com.mundox.management.ports.mocks.adapters.SnackServiceMock
+import monix.eval.Task
 
 class TestEnvironment extends Environment {
 
   val serviceMock: DummyMoviesService = getMock
 
   override val dummyMoviesService: DummyMoviesService = serviceMock
-  override val snacksService: SnacksService[Future] = ???
+  override val snacksService: SnacksService[Task] = new SnackServiceMock
 }
 
 object TestEnvironment extends TestEnvironment
---
+++ management/ports/src/test/scala/com/mundox/management/ports/mocks/adapters/SnackServiceMock.scala
@@ -0,0 +1,20 @@
+package com.mundox.management.ports.mocks.adapters
+
+import cats.data.EitherT
+import com.mundox.management.core.services.SnacksService
+import monix.eval.Task
+import com.mundox.management.core.domain.snacks.{Snack, SnackType}
+
+class SnackServiceMock extends SnacksService[Task] {
+
+  override def getSnacks: EitherT[Task, String, List[Snack]] =
+    EitherT.cond[Task](
+      true,
+      List(Snack(123, "Chocolate", SnackType.Sweet, 1, 10.20), Snack(432, "Potato chips", SnackType.Salty, 100, 4.50)),
+      "Error retrieving the snacks information"
+    )
+
+  override def getSnacksById(id: Int): Task[Option[Snack]] = Task {
+    Option(Snack(123, "Chocolate", SnackType.Sweet, 1, 10.20))
+  }
+}
