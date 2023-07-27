50f4b545680ce1934ef9a15284e294718ac223b0
Milenio X <jnkmiloq@gmail.com>
2023-07-27 09:14:12 -0500

    Add Snack routes tests

---
+++ management/ports/src/test/scala/com/mundox/management/ports/api/http/routes/SnackRoutesTest.scala
@@ -0,0 +1,29 @@
+package com.mundox.management.ports.api.http.routes
+
+import akka.http.scaladsl.model.MediaTypes
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.headers.`Content-Type`
+import com.mundox.management.ports.api.http.responses.SnackResponseDTO
+import com.mundox.management.ports.{TestEnvironment, TestSpec}
+import com.mundox.management.ports.api.http.{JsonSupport, ManagementAPI}
+
+class SnackRoutesTest extends TestSpec with JsonSupport {
+
+  val apiRoutes: ManagementAPI = new ManagementAPI(TestEnvironment)
+
+  "The get snacks service" should "return a list of snacks" in {
+    Get("/management/snacks") ~> apiRoutes.routes ~> check {
+      status shouldEqual OK
+      header("Content-Type").getOrElse("Empty") shouldEqual `Content-Type`(MediaTypes.`application/json`)
+      responseAs[List[SnackResponseDTO]] shouldEqual List(SnackResponseDTO(123, "Chocolate", 10.20), SnackResponseDTO(432, "Potato chips", 4.50))
+    }
+  }
+
+  "The get snacks by id service" should "return a snack" in {
+    Get("/management/snacks/123") ~> apiRoutes.routes ~> check {
+      status shouldEqual OK
+      header("Content-Type").getOrElse("Empty") shouldEqual `Content-Type`(MediaTypes.`application/json`)
+      responseAs[SnackResponseDTO] shouldEqual SnackResponseDTO(123, "Chocolate", 10.20)
+    }
+  }
+}
